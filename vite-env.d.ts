d62347e2d8bc638e1a80757064c66e8df7e137eb
Hiro <laciferin@gmail.com>
2025-10-23 16:34:35 +0530

    ready: vite env claenup

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,11 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APTOS_NODE_URL: string
-  readonly VITE_MONEY_POT_CONTRACT_ADDRESS: string
   readonly VITE_MONEY_AUTH_URL: string
-  readonly VITE_VERIFIER_SERVICE_BASE_URL: string
-  readonly VITE_USDC_TOKEN_ADDRESS: string
 }
 
 interface ImportMeta {
