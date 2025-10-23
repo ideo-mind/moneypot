31c055291376bead0d63cd864c10057df722f3d8
Hiro <laciferin@gmail.com>
2025-10-23 16:32:52 +0530

    migrate: vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { cloudflare } from '@cloudflare/vite-plugin'
 import path from 'path'
 
 // Vite recommends exporting a function to access mode
@@ -10,9 +9,6 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [
       react(),
-      cloudflare({
-        configPath: './wrangler.toml',
-      }),
     ],
     resolve: {
       alias: {
